98b1dfa666ee814ae775e177e4310b8dba98f375
Nodirbek3628 <abloqulovnodirbek4@gmail.com>
2025-08-31 12:36:51 +0500

    decorators

--- Decorators.py
+++ Decorators.py
@@ -1,11 +1,29 @@
-def chek_item(func):
-    def wrapper(nums: list):
+# def chek_item(func):
+#     def wrapper(nums: list):
 
-        if any([type(item) not in (int,float) for item in nums]):
-            raise Exception("faqat son kiriting")
+#         if any([type(item) not in (int,float) for item in nums]):
+#             raise Exception("faqat son kiriting")
         
-        result = func(nums)
+#         result = func(nums)
 
-        return result
+#         return result
+#     return wrapper
+
+
+
+
+def requers_login(func):
+    user_name = "ali"
+    user_password = "1"
+
+    def wrapper():
+        username = input("username: ")
+        password = input("password: ")
+
+        if username != user_name or user_password!= password :
+            print("Login qilishingz shart")
+        else:
+            func()
+
     return wrapper
 
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from Decorators import chek_item
+from Decorators import requers_login
 
-@chek_item
-def yigindi(nums):
-    return sum(nums)
+# @chek_item
+# def yigindi(nums):
+#     return sum(nums)
 
-print(yigindi([1,2,2457]))
+# print(yigindi([1,2,2457]))
+
+
+@requers_login
+def show_profil():
+    print("Your profil")
+
+
+show_profil()
